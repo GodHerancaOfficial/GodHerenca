d38ea2161f14f2e6a6319525f72e7d5af400dd60
Praisecodes <cmancoder@gmail.com>
2023-02-06 18:17:24 +0100

    [Update] Updating context

--- contexts/SetupContext.tsx
+++ contexts/SetupContext.tsx
@@ -1,18 +1,21 @@
 import { createContext, useState } from "react";
 
 type SetupContextType= {
-    fullName?: string,
-    setFullName?: any,
-    username?: string,
-    setUsername?: any,
-    gender?: string,
-    setGender?: any,
-    cpf?: string,
-    setCpf?: any,
-    phone?: string,
-    setPhone?: any,
-    accountType?: string,
-    setAccountType?: any,
+    fullName?: string, setFullName?: any,
+    username?: string, setUsername?: any,
+    gender?: string, setGender?: any,
+    cpf?: string, setCpf?: any,
+    phone?: string, setPhone?: any,
+    accountType?: string, setAccountType?: any,
+    dob?: string, setDob?: any,
+    postalCode?: string, setPostalCode?: any,
+    state?: string, setState?: any,
+    city?: string, setCity?: any,
+    address?: string, setAddress?: any,
+    guarantorName?: string, setGuarantorName?: any,
+    guarantorPhone?: string, setGuarantorPhone?: any,
+    guarantorRelationship?: string, setGuarantorRelationship?: any,
+    selectedVehicle?: string, setSelectedVehicle?: any
 }
 
 const SetupContext = createContext<SetupContextType>({});
@@ -24,6 +27,15 @@ export const ContextProvider = ({ children }: any) => {
     const [cpf, setCpf] = useState<string>("");
     const [phone, setPhone] = useState<string>("");
     const [accountType, setAccountType] = useState<string>("");
+    const [dob, setDob] = useState<string>("");
+    const [postalCode, setPostalCode] = useState<string>("");
+    const [state, setState] = useState<string>("");
+    const [city, setCity] = useState<string>("");
+    const [address, setAddress] = useState<string>("");
+    const [guarantorName, setGuarantorName] = useState<string>("");
+    const [guarantorPhone, setGuarantorPhone] = useState<string>("");
+    const [guarantorRelationship, setGuarantorRelationship] = useState<string>("");
+    const [selectedVehicle, setSelectedVehicle] = useState<string>("");
 
     const values:SetupContextType = {
         fullName, setFullName,
@@ -31,7 +43,16 @@ export const ContextProvider = ({ children }: any) => {
         gender, setGender,
         cpf, setCpf,
         phone, setPhone,
-        accountType, setAccountType
+        accountType, setAccountType,
+        dob, setDob,
+        postalCode, setPostalCode,
+        state, setState,
+        city, setCity,
+        address, setAddress,
+        guarantorName, setGuarantorName,
+        guarantorPhone, setGuarantorPhone,
+        guarantorRelationship, setGuarantorRelationship,
+        selectedVehicle, setSelectedVehicle
     }
 
     return (
