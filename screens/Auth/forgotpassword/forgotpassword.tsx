43656d260abb554718a379e7699c67b25289ec05
Praisecodes <cmancoder@gmail.com>
2023-02-26 18:00:49 +0100

    [Update] Update forgot password screen

--- screens/Auth/forgotpassword/forgotpassword.tsx
+++ screens/Auth/forgotpassword/forgotpassword.tsx
@@ -1,31 +1,67 @@
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableWithoutFeedback, View } from "react-native";
 import { ForgotPasswordLayout } from "../../../layouts";
 import { ForgotLayoutStyle } from "../../../styles/Auth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function ForgotPassword({ navigation }: any): any {
     const imageSrc = require('../../../assets/images/forgot_password/screen1.png');
-    const [contactDetails, setContactDetails] = useState<any[]>([
+    const [selectedMethod, setSelectedMethod] = useState<string>('');
+    const [btnActive, setBtnActive] = useState<boolean>(false);
+    const [contactDetails] = useState<any[]>([
         {
             id: 1,
             userDetail: '+234 123 *** 840',
+            type: 'Sms',
         },
         {
             id: 2,
             userDetail: 'cman****@gmail.com',
+            type: 'Email',
         },
     ]);
+
+    useEffect(()=>{
+        ((selectedMethod=="")?setBtnActive(false):setBtnActive(true));
+    },[selectedMethod])
 
     return (
         <ForgotPasswordLayout navigateBack={navigation.pop}>
             <View style={[ForgotLayoutStyle.contentContainer]}>
                 <Image source={imageSrc} style={ForgotLayoutStyle.imageStyle} />
 
-                <Text style={{fontFamily: 'Raleway', fontSize: 16,}}>
+                <Text style={{ fontFamily: 'Raleway', fontSize: 15, }}>
                     {'\n'}
                     Select which contact details you wish to use in
                     resetting your password
                 </Text>
+                <View style={{ marginVertical: 30, width: '85%' }}>
+                    {
+                        contactDetails.map((contactDetail) => (
+                            <TouchableWithoutFeedback key={contactDetail.id} onPress={()=>{setSelectedMethod(contactDetail.type)}}>
+                                <View style={[ForgotLayoutStyle.contactDetail, {borderColor: (selectedMethod==contactDetail.type)?'#000000':'#DEDEDE', borderWidth: (selectedMethod==contactDetail.type)?2:1 }]}>
+                                    <MaterialIcons
+                                        name={(contactDetail.type == "Sms") ? 'sms' : "email"}
+                                        size={27}
+                                        color="black"
+                                        style={{ backgroundColor: '#EDEDED', padding: 13, borderRadius: 100, }}
+                                    />
+
+                                    <Text style={{fontFamily:'Raleway', fontSize: 16, paddingHorizontal: 20}}>
+                                        Via {contactDetail.type} {'\n'}
+                                        <Text style={{fontFamily: 'Raleway-bold', fontSize: 18,}}>{contactDetail.userDetail}</Text>
+                                    </Text>
+                                    <View></View>
+                                </View>
+                            </TouchableWithoutFeedback>
+                        ))
+                    }
+                </View>
+                <TouchableWithoutFeedback>
+                    <Text style={[ForgotLayoutStyle.btnStyle, {opacity: (btnActive)?1:0.5}]}>
+                        CONTINUE
+                    </Text>
+                </TouchableWithoutFeedback>
             </View>
         </ForgotPasswordLayout>
     )
--- styles/Auth/forgot_layout_style.tsx
+++ styles/Auth/forgot_layout_style.tsx
@@ -19,6 +19,28 @@ const ForgotLayoutStyle = StyleSheet.create({
         overflow: 'hidden',
         resizeMode: 'contain',
     },
+    contactDetail: {
+        width: '100%',
+        padding: 20,
+        borderColor: '#DEDEDE',
+        borderWidth: 1,
+        marginVertical: 15,
+        borderRadius: 15,
+        display: 'flex',
+        // justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    btnStyle: {
+        backgroundColor: '#000',
+        paddingVertical: 16,
+        width: '85%',
+        fontFamily: 'Raleway-bold',
+        color: '#fff',
+        textAlign: 'center',
+        borderRadius: 100,
+        fontSize: 16,
+    }
 });
 
 export default ForgotLayoutStyle;
