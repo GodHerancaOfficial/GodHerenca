8d2fddf483eece04d0ffe5dcaa9e41392b3fbbbb
Praisecodes <cmancoder@gmail.com>
2023-03-10 13:50:21 +0100

    [Update] Update Create New Password Screen

--- components/common/input.tsx
+++ components/common/input.tsx
@@ -3,21 +3,26 @@ import {
     Text,
     TouchableWithoutFeedback,
     TextInput,
-    KeyboardTypeOptions
+    KeyboardTypeOptions,
+    StyleSheet
 } from "react-native";
-import { MaterialIcons } from "@expo/vector-icons";
-// import { Icon } from "@expo/vector-icons/build/createIconSet";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useState } from "react";
 
 interface InputProps {
     placeholder: string
-    icon?: any,
+    icon?: any | undefined,
     keyboardType?: KeyboardTypeOptions,
     secureTextEntry?: boolean,
+    onChangeText: any,
+    style?: any | undefined,
 }
 
-export default function Input({ placeholder, icon, keyboardType, secureTextEntry }: InputProps): any {
+export default function Input({ placeholder, icon, keyboardType, secureTextEntry, onChangeText, style }: InputProps): any {
+    const [viewPassword, setViewPassword] = useState<boolean>(false);
+
     return (
-        <View>
+        <View style={InputStyle.container || style}>
             {/* Icon To Be placed on the input */}
             {icon}
 
@@ -25,14 +30,39 @@ export default function Input({ placeholder, icon, keyboardType, secureTextEntry
             <TextInput
                 placeholder={placeholder}
                 keyboardType={keyboardType}
-                secureTextEntry={secureTextEntry}
+                secureTextEntry={((viewPassword) ? false : secureTextEntry)}
                 style={{
                     fontFamily: 'Raleway',
-                    fontSize: 16,
+                    fontSize: 18,
+                    alignSelf: 'stretch',
+                    width: ((icon) ? '75%' : '90%')
                 }}
+                onChangeText={onChangeText}
             />
 
             {/* View Icon, in case input is for a password. */}
+            {((secureTextEntry) ?
+                <TouchableWithoutFeedback onPress={() => { setViewPassword(!viewPassword) }}>
+                    <MaterialCommunityIcons name="eye-off" color={'#A5A5A5'} size={20} />
+                </TouchableWithoutFeedback>
+                :
+                <View></View>
+            )}
         </View>
     )
 }
+
+const InputStyle = StyleSheet.create({
+    container: {
+        backgroundColor: '#F6F6F6',
+        width: '100%',
+        marginVertical: 10,
+        borderRadius: 7,
+        paddingHorizontal: 10,
+        paddingVertical: 15,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    }
+});
--- screens/Auth/forgotpassword/createNewPassword.tsx
+++ screens/Auth/forgotpassword/createNewPassword.tsx
@@ -3,16 +3,48 @@ import { ForgotPasswordLayout } from "../../../layouts";
 import { ForgotLayoutStyle } from "../../../styles/Auth";
 import { Button, Input } from "../../../components/common";
 import { MaterialIcons } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
+import { GeneralModal } from "../../../components/modals";
 
 export default function CreateNewPassword({ navigation }: any): any {
     const image = require('../../../assets/images/forgot_password/screen2.png');
+    const [newPassword, setNewPassword] = useState<string>("");
+    const [confirmPassword, setConfirmPassword] = useState<string>("");
+    const [btnDisabled, setBtnDisabled] = useState<boolean>(true);
+    const [visible, setVisible] = useState<boolean>(false);
+    const imageSource = require('../../../assets/images/modals/changePwd.png');
+
+    useEffect(() => {
+        if (newPassword == "" || confirmPassword == "" || confirmPassword != newPassword) {
+            setBtnDisabled(true);
+            return;
+        }
+
+        setBtnDisabled(false);
+    }, [newPassword, confirmPassword]);
 
     return (
         <ForgotPasswordLayout navigateBack={navigation.pop}>
+            <GeneralModal visible={visible} setVisible={setVisible} imageSource={imageSource} >
+                <Text style={[ForgotLayoutStyle.text, {
+                    fontFamily: 'Raleway-bold',
+                    fontSize: 24,
+                    textAlign: 'center',
+                    paddingHorizontal: 20
+                }]}>
+                    Congratulations! {'\n\n'}
+
+                    <Text style={[ForgotLayoutStyle.text, { fontSize: 16, paddingVertical: 15 }]}>
+                        Your password was successfully changed. You will be redirected to your
+                        login page in few seconds.
+                    </Text>
+                </Text>
+            </GeneralModal>
+
             <View style={[ForgotLayoutStyle.contentContainer, { paddingHorizontal: 20 }]}>
                 <Image source={image} style={[ForgotLayoutStyle.imageStyle]} />
 
-                <Text style={[ForgotLayoutStyle.text, { width: '100%', marginVertical: 20 }]}>
+                <Text style={[ForgotLayoutStyle.text, { width: '100%', marginVertical: 30 }]}>
                     Create Your New Password
                 </Text>
 
@@ -22,12 +54,23 @@ export default function CreateNewPassword({ navigation }: any): any {
                     icon={
                         <MaterialIcons name="lock" size={24} color={'#000'} />
                     }
+                    onChangeText={(e: string) => { setNewPassword(e) }}
+                />
+
+                <Input
+                    placeholder="Confirm Password"
+                    secureTextEntry={true}
+                    icon={
+                        <MaterialIcons name="lock" size={24} color={'#000'} />
+                    }
+                    onChangeText={(e: string) => { setConfirmPassword(e) }}
                 />
 
                 <Button
-                    onPress={() => { }}
-                    disabled={true}
-                    style={[ForgotLayoutStyle.btnStyle, { width: '100%' }]}>
+                    onPress={() => { setVisible(!visible) }}
+                    disabled={btnDisabled}
+                    style={[ForgotLayoutStyle.btnStyle, { width: '100%', position: 'absolute', bottom: 20 }]}
+                >
                     Continue
                 </Button>
             </View>
