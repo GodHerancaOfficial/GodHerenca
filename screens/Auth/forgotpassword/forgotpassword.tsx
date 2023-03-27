fe34c5ca5167bc1f336c24a34a8bb963449399d5
Praisecodes <cmancoder@gmail.com>
2023-03-27 14:20:44 +0100

    Push

--- screens/Auth/forgotpassword/forgotpassword.tsx
+++ screens/Auth/forgotpassword/forgotpassword.tsx
@@ -12,13 +12,13 @@ export default function ForgotPassword({ navigation }: any): any {
     const [contactDetails] = useState<any[]>([
         {
             id: 1,
-            userDetail: '+234 123 *** 840',
-            type: 'phone',
+            contactDetail: '+234 123 *** 840',
+            contactType: 'phone',
         },
         {
             id: 2,
-            userDetail: 'cman****@gmail.com',
-            type: 'email',
+            contactDetail: 'cman****@gmail.com',
+            contactType: 'email',
         },
     ]);
 
@@ -39,18 +39,18 @@ export default function ForgotPassword({ navigation }: any): any {
                 <View style={{ marginVertical: 30, width: '85%' }}>
                     {
                         contactDetails.map((contactDetail) => (
-                            <TouchableWithoutFeedback key={contactDetail.id} onPress={() => { setSelectedMethod(contactDetail.type) }}>
-                                <View style={[ForgotLayoutStyle.contactDetail, { borderColor: (selectedMethod == contactDetail.type) ? '#000000' : '#DEDEDE', borderWidth: (selectedMethod == contactDetail.type) ? 2 : 1 }]}>
+                            <TouchableWithoutFeedback key={contactDetail.id} onPress={() => { setSelectedMethod(contactDetail.contactType) }}>
+                                <View style={[ForgotLayoutStyle.contactDetail, { borderColor: (selectedMethod == contactDetail.contactType) ? '#000000' : '#DEDEDE', borderWidth: (selectedMethod == contactDetail.type) ? 2 : 1 }]}>
                                     <MaterialIcons
-                                        name={(contactDetail.type == "phone") ? 'sms' : "email"}
+                                        name={(contactDetail.contactType == "phone") ? 'sms' : "email"}
                                         size={27}
                                         color="black"
                                         style={{ backgroundColor: '#EDEDED', padding: 13, borderRadius: 100, }}
                                     />
 
                                     <Text style={{ fontFamily: 'Raleway', fontSize: 16, paddingHorizontal: 20 }}>
-                                        Via {(contactDetail.type == "email") ? 'Email' : 'Sms'} {'\n'}
-                                        <Text style={{ fontFamily: 'Raleway-bold', fontSize: 18, }}>{contactDetail.userDetail}</Text>
+                                        Via {(contactDetail.contactType == "email") ? 'Email' : 'Sms'} {'\n'}
+                                        <Text style={{ fontFamily: 'Raleway-bold', fontSize: 18, }}>{contactDetail.contactDetail}</Text>
                                     </Text>
                                     <View></View>
                                 </View>
