5b4ebfec7d3584193f18b05da784226a072133bf
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-19 13:27:45 +0100

    Implemented modal for account setup success

--- screens/accountSetup/origin.tsx
+++ screens/accountSetup/origin.tsx
@@ -5,87 +5,119 @@ import DropDown from "../../components/dropdown/dropdown";
 import { useEffect, useState, useContext } from "react";
 import SetupContext from "../../contexts/SetupContext";
 import { Button } from "../../components/common";
+import { GeneralModal } from "../../components/modals";
+import { ForgotLayoutStyle } from "./../../styles/Auth";
 
 export default function OriginScreen({ navigation }: any): any {
-    const [btnActive, setBtnActive] = useState<boolean>(true);
+  const [btnActive, setBtnActive] = useState<boolean>(true);
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const modalImageSource = require("./../../assets/images/modals/regSuccess.png");
 
-    // const { state, city, address, setState, setCity, setAddress } = useContext(SetupContext);
-    const { detailsObj, setDetailsObj } = useContext(SetupContext);
+  // const { state, city, address, setState, setCity, setAddress } = useContext(SetupContext);
+  const { detailsObj, setDetailsObj } = useContext(SetupContext);
 
-    useEffect(() => {
-        if (detailsObj.state == "" || detailsObj.city == "" || detailsObj.address == "") {
-            setBtnActive(true);
-            return;
-        }
-
-        setBtnActive(false);
-    }, [detailsObj.state, detailsObj.city, detailsObj.address]);
-
-    const handleBtnPress = (): void | null => {
-        console.log(detailsObj);
-
-        ((detailsObj.accountType == "Rider") ? navigation.navigate('Guarantor') : null);
+  useEffect(() => {
+    if (
+      detailsObj.state == "" ||
+      detailsObj.city == "" ||
+      detailsObj.address == ""
+    ) {
+      setBtnActive(true);
+      return;
     }
 
-    return (
-        <AccountSetupLayout navigateBack={navigation.pop}>
-            <View style={SetupStyle.generalView}>
-                <Text style={SetupStyle.titleText}>
-                    State
-                </Text>
+    setBtnActive(false);
+  }, [detailsObj.state, detailsObj.city, detailsObj.address]);
 
-                <DropDown
-                    placeholder="Select State"
-                    lists={[{ id: 1, title: "Coming Soon!" }]}
-                    onChange={(choice: string) => {
-                        setDetailsObj((detailsObj: any) => ({
-                            ...detailsObj,
-                            'state': choice
-                        }))
-                    }}
-                    value={detailsObj.state}
-                />
+  const handleBtnPress = (): void | null => {
+    console.log(detailsObj);
 
-                <Text style={SetupStyle.titleText}>
-                    City
-                </Text>
+    detailsObj.accountType == "Rider"
+      ? navigation.navigate("Guarantor")
+      : setModalVisible(!modalVisible);
+  };
 
-                <DropDown
-                    placeholder="Select City"
-                    lists={[{ id: 1, title: "Coming Soon!" }]}
-                    onChange={(choice: string) => {
-                        setDetailsObj((detailsObj: any) => ({
-                            ...detailsObj,
-                            'city': choice
-                        }))
-                    }}
-                    value={detailsObj.city}
-                />
+  return (
+    <AccountSetupLayout navigateBack={navigation.pop}>
+      <GeneralModal
+        visible={modalVisible}
+        setVisible={setModalVisible}
+        imageSource={modalImageSource}
+      >
+        <Text
+          style={[
+            ForgotLayoutStyle.text,
+            {
+              fontFamily: "Raleway-bold",
+              fontSize: 24,
+              textAlign: "center",
+              paddingHorizontal: 20,
+            },
+          ]}
+        >
+          Congratulations! {"\n\n"}
+          <Text
+            style={[
+              ForgotLayoutStyle.text,
+              { fontSize: 16, paddingVertical: 15 },
+            ]}
+          >
+            Your account is ready to use. you will will be redirected to your
+            home page in a few seconds
+          </Text>
+        </Text>
+      </GeneralModal>
+      <View style={SetupStyle.generalView}>
+        <Text style={SetupStyle.titleText}>State</Text>
 
-                <Text style={SetupStyle.titleText}>
-                    Address
-                </Text>
+        <DropDown
+          placeholder="Select State"
+          lists={[{ id: 1, title: "Coming Soon!" }]}
+          onChange={(choice: string) => {
+            setDetailsObj((detailsObj: any) => ({
+              ...detailsObj,
+              state: choice,
+            }));
+          }}
+          value={detailsObj.state}
+        />
 
-                <TextInput
-                    placeholder="Your Address"
-                    style={SetupStyle.formInputs}
-                    onChangeText={(e) => {
-                        setDetailsObj((detailsObj: any) => ({
-                            ...detailsObj,
-                            'address': e
-                        }));
-                    }}
-                    value={detailsObj.address}
-                />
+        <Text style={SetupStyle.titleText}>City</Text>
 
-                <Button
-                    style={SetupStyle.button}
-                    onPress={() => handleBtnPress()}
-                    disabled={btnActive}
-                >
-                    {(detailsObj.accountType == 'Rider') ? 'Next' : 'Finish'}
-                </Button>
-            </View>
-        </AccountSetupLayout>
-    )
+        <DropDown
+          placeholder="Select City"
+          lists={[{ id: 1, title: "Coming Soon!" }]}
+          onChange={(choice: string) => {
+            setDetailsObj((detailsObj: any) => ({
+              ...detailsObj,
+              city: choice,
+            }));
+          }}
+          value={detailsObj.city}
+        />
+
+        <Text style={SetupStyle.titleText}>Address</Text>
+
+        <TextInput
+          placeholder="Your Address"
+          style={SetupStyle.formInputs}
+          onChangeText={(e) => {
+            setDetailsObj((detailsObj: any) => ({
+              ...detailsObj,
+              address: e,
+            }));
+          }}
+          value={detailsObj.address}
+        />
+
+        <Button
+          style={SetupStyle.button}
+          onPress={() => handleBtnPress()}
+          disabled={btnActive}
+        >
+          {detailsObj.accountType == "Rider" ? "Next" : "Finish"}
+        </Button>
+      </View>
+    </AccountSetupLayout>
+  );
 }
